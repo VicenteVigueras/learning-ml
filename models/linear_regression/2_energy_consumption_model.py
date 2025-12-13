038b3ff661b4b46c685023676f456f880cb811c3
VicenteVigueras <vicente.viguerassilva@gmail.com>
2025-12-13 15:56:48 -0500

    feat: applying linear regression pattern to train data on Square Footage vs Energy Consumption

--- models/linear_regression/2_energy_consumption_model.py
+++ models/linear_regression/2_energy_consumption_model.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
 import pandas as pd
 
 """
@@ -5,7 +8,7 @@ TODO: Apply linear regression to energy consumption dataset
 Understand how features like temperature, humidity, and time of day affect energy usage
 """
 
-df = pd.read_csv("models/linear_regression/data/test_energy_data.csv")
+df = pd.read_csv("models/linear_regression/data/train_energy_data.csv")
 for column in df.columns:
     print(column)
 
@@ -21,3 +24,18 @@ print(df.head())
 6. Days of the week : Categorical feature representing whether the data point corresponds to a weekday or weekend.
 7. Energy Consumption : Numeric target variable representing the energy consumption of the building in kWh (kilowatt-hours). This is the value
 """
+
+
+# Based on this I'll do Square Footage vs Energy Consumption
+
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs = np.array(df["Square Footage"], dtype=float)
+ys = np.array(df["Energy Consumption"], dtype=float)
+
+model.fit(xs, ys, epochs=500)
+
+w, b = model.layers[0].get_weights()
+print(f"weight: {w}, intercept{b}")
